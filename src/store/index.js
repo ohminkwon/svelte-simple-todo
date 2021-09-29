15052beb80ef985b9bc727eb3be7bfebbf5910bc
ohminkwon <ohminkwon0720@gmail.com>
2021-09-29 21:21:36 +0900

    revise semi-colon

---
+++ src/store/index.js
@@ -0,0 +1,7 @@
+import { writable, get } from 'svelte/store'
+
+// get(todos) === $todos
+export const todos = writable([])
+export function saveStorage(){
+  localStorage.setItem('todos', JSON.stringify(get(todos)))
+}
